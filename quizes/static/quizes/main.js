032394420a3478ad4ca7f4ac2d4b73ab6c1a904d
ursorathi <sorathiunnati@gmail.com>
2022-10-07 23:19:55 +0530

    Modified files

--- quizes/static/quizes/main.js
+++ quizes/static/quizes/main.js
@@ -16,10 +16,10 @@ modalBtns.forEach(modalBtn=> modalBtn.addEventListener('click', ()=>{
         <div class="h5 mb-3">Are you sure you want to begin "<b>${name}</b>"?</div>
         <div class="text-muted">
             <ul>
-                <li>difficulty: <b>${difficulty}</b></li>
-                <li>number of questions: <b>${numQuestions}</b></li>
-                <li>score to pass: <b>${scoreToPass}%</b></li>
-                <li>time: <b>${time} min</b></li>
+                <li>Difficulty level: <b>${difficulty}</b></li>
+                <li>Number of questions: <b>${numQuestions}</b></li>
+                <li>Score to pass: <b>${scoreToPass}%</b></li>
+                <li>Time: <b>${time} min</b></li>
             </ul>
         </div>
     `
